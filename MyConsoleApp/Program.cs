f9f00adacb3a3fed3f90a370124d5e15e444f867
JEISON1221037004 <jeisonmanuyama@gmail.com>
2024-10-16 17:39:37 -0500

    Progrm

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -1,10 +1,14 @@
-﻿namespace MyConsoleApp
+﻿using MyLibrary;
+
+namespace MyConsoleApp
 {
     internal class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Hello, World!");
+            MessageWriter MyWriter = new MessageWriter();
+            MyWriter.Write("Hello Writer!");
         }
     }
 }
