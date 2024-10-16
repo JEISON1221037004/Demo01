c47af3d26a5f9442fb2ee4e4cf21dd52ccfd3a73
JEISON1221037004 <jeisonmanuyama@gmail.com>
2024-10-16 17:37:45 -0500

    MessageWriter

---
+++ MyLibrary/MessageWriter.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+
+    public class MessageWriter
+    {
+        public void Write(string message)
+        {
+            Console.WriteLine(message);
+        }
+    }
+}
