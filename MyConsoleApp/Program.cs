cad60e5e8ffac64a8915387e8a4bcc3edcfa4f5a
JEISON1221037004 <jeisonmanuyama@gmail.com>
2024-10-16 17:43:28 -0500

    Program

--- MyConsoleApp/Program.cs
+++ MyConsoleApp/Program.cs
@@ -9,6 +9,8 @@ namespace MyConsoleApp
             Console.WriteLine("Hello, World!");
             MessageWriter MyWriter = new MessageWriter();
             MyWriter.Write("Hello Writer!");
+            FiggleWriter FWriter = new FiggleWriter();
+            FWriter.Write("Hello, Figgle!!!");
         }
     }
 }
